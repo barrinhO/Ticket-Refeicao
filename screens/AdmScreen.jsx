55b65afaaa08bcdf60708e6fca73ce3fcd5ce55a
CHRISTIAN MOREIRA DE BARROS <christian_m_barros@estudante.sesisenai.org.br>
2025-08-25 15:25:44 -0300

    montando a tela do adm

--- screens/AdmScreen.jsx
+++ screens/AdmScreen.jsx
@@ -1,1 +1,46 @@
-adadada;
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+export default function AdmScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Administrador</Text>
+
+      {/* botões */}
+
+      <TouchableOpacity>
+        <Text style={styles.buttonText}>Cadastrar alunos</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity>
+        <Text style={styles.buttonText}>Visualizar tickets utilizados</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity>
+        <Text style={styles.buttonText}>Histórico de Tickets</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity>
+        <Text style={styles.buttonText}>Resetar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f5f5f5",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  buttonText: {
+    fontSize: 18,
+    color: "#007bff",
+    marginVertical: 10,
+  },
+});
